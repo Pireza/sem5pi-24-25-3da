f4648163797abafb5b0d7da2744b643350ea552c
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-10-02 14:37:44 +0100

    Relational DB Integration

---
+++ TodoApi/Models/User.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string? Email { get; set; }
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Import the correct MySQL namespace
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Configure MySQL as the database provider
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("UserContext"), 
+        new MySqlServerVersion(new Version(8, 0, 39)) // Replace with the correct MySQL version
+    )
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
