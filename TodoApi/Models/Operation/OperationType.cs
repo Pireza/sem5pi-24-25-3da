8ac9b21ceea4331a9dc3c9bae04c727f59c4bb1a
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-10-24 15:49:57 +0100

    Operation Type tests

--- TodoApi/Models/Operation/OperationType.cs
+++ TodoApi/Models/Operation/OperationType.cs
@@ -1,11 +1,36 @@
+using System.Text.RegularExpressions;
 using Microsoft.EntityFrameworkCore;
 
 [Index(nameof(Name), IsUnique = true)]
 public class OperationType
 {
     public long Id { get; set; }
-    public required string Name { get; set; }
-    public required string Duration { get; set; }
-    public required string Status { get; set; }
+    public string Name { get; set; }
+    public string Duration { get; set; }
+    public string Status { get; set; }
     public bool IsActive { get; set; } = true; // Add an IsActive flag
+    public OperationType() { }
+    public OperationType(string Name, string Duration, string Status)
+    {
+        if (ValidDuration(Duration))
+        {
+            this.Name = Name;
+            this.Duration = Duration;
+            this.Status = Status;
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+    private bool ValidDuration(string duration)
+    {
+        string DurationPattern = @"^([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$";
+        Regex regex = new Regex(DurationPattern);
+
+        if (!regex.IsMatch(duration))
+            return false;
+
+        return true;
+    }
 }
---
+++ TodoApi/Tests/OperationTypeTest.cs
@@ -0,0 +1,25 @@
+using TodoApi.Models;
+
+public class OperationTypeTest
+{
+    [Theory]
+    [InlineData("First", "01:00:00", "Status")]
+    [InlineData("Second", "02:30:00", "Status")]
+    [InlineData("Third", "03:45:15", "Status")]
+    public void WhenCorrectDataIsPassed_OperationTypeIsCreated(string Name, string Duration, string Status)
+    {
+        _ = new OperationType(Name, Duration, Status);
+    }
+
+    [Theory]
+    [InlineData("First", "1:00:00", "Status")]
+    [InlineData("Second", "02:3:00", "Status")]
+    [InlineData("Third", "03:45:5", "Status")]
+    [InlineData("Fourth", "04:45", "Status")]
+    public void WhenInvalidDataIsPassed_ExceptionIsThrown(string name, string duration, string status)
+    {
+        Assert.Throws<ArgumentException>(() =>
+                   _ = new OperationType(name, duration, status)
+               );
+    }
+}
