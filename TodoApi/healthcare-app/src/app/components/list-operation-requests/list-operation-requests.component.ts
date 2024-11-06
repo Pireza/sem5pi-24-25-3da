672a0f2b50a4c7ab5cd1ebc6c62b3b0b8601f7ab
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-11-06 08:42:00 +0000

    #37 Setting up angular components

---
+++ TodoApi/healthcare-app/src/app/components/list-operation-requests/list-operation-requests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-operation-requests',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-operation-requests.component.html',
+  styleUrl: './list-operation-requests.component.css'
+})
+export class ListOperationRequestsComponent {
+
+}
