d29604fa3e0ba20c0a77a70a00701fc18cf8988d
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-11-06 08:27:55 +0000

    #35 Adding operation in/success pop-up

--- TodoApi/healthcare-app/src/app/components/reset-password/reset-password.component.ts
+++ TodoApi/healthcare-app/src/app/components/reset-password/reset-password.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
-  email: String = ''; 
+  email: String = '';
+  message: string = '';
+  isSuccess: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit() {
     this.authService.resetPassword(this.email).subscribe({
-      error: (error) => {
-        console.error('Error sending email:', error);
+      next: (response: any) => {
+        this.isSuccess = true;
+        this.message = response.message || "Password reset email sent successfully";
+      },
+      error: (error: HttpErrorResponse) => {
+        this.isSuccess = false;
+        this.message = error.error?.message || "Failed to send password reset email";
       }
     });
   }
