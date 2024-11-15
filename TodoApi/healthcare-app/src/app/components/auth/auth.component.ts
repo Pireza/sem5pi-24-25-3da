8599eb2fcbd7dd42361f6e426fa612def0a969b6
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-11-15 18:29:30 +0000

    #35 Reworking Reset Password GUI

--- TodoApi/healthcare-app/src/app/components/auth/auth.component.ts
+++ TodoApi/healthcare-app/src/app/components/auth/auth.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router'; // Import Router
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/userService'; // Import UserService
-
+import { ResetPasswordComponent } from '../reset-password/reset-password.component'; // Import the reset-password component
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
+  standalone: true,
+  imports: [ResetPasswordComponent, CommonModule]
 })
 export class AuthComponent {
   userEmail: string | null = null; // To hold the user's email
   userRole: string | null = null;   // To hold the user's role
+  isPasswordResetMode: boolean = false; // Control the visibility of the reset password form
+
   constructor(private authService: AuthService, private router: Router, private userService: UserService) { } // Inject Router
 
   onAuthenticate() {
@@ -42,8 +47,8 @@ export class AuthComponent {
     this.router.navigate(['/registerClient']);
   }
 
+  // Toggle the password reset form visibility
   onPasswordReset() {
-    this.router.navigate(['/reset-password'])
+    this.isPasswordResetMode = true;
   }
-
 }
