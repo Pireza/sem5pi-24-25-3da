37abe80c7749530a33e6933b44c836dcd651304c
Pedro Pires <161972196+Pireza@users.noreply.github.com>
2024-11-15 19:57:15 +0000

    Changing authenticated GUI

--- TodoApi/healthcare-app/src/app/components/auth/auth.component.ts
+++ TodoApi/healthcare-app/src/app/components/auth/auth.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/userService'; // Import UserService
 import { ResetPasswordComponent } from '../reset-password/reset-password.component'; // Import the reset-password component
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -15,40 +16,39 @@ export class AuthComponent {
   userEmail: string | null = null; // To hold the user's email
   userRole: string | null = null;   // To hold the user's role
   isPasswordResetMode: boolean = false; // Control the visibility of the reset password form
+  isAuthenticated: boolean = false; // Authentication state
+  isSidebarOpen: boolean = false; // Sidebar visibility state
 
-  constructor(private authService: AuthService, private router: Router, private userService: UserService) { } // Inject Router
+  constructor(private authService: AuthService, private router: Router, private userService: UserService) { }
 
+  // Authenticate user
   onAuthenticate() {
     this.authService.authenticateUser().subscribe({
       next: (response) => {
-        // Check authentication status and update user role after authentication
         if (this.authService.isAuthenticated) {
-          this.userEmail = this.userService.userEmail; // Update email here
-          this.userRole = this.userService.userRole;   // Update role here
-
-          // Now check the userRole
-          if (this.userRole === 'Patient') {
-            this.router.navigate(['/patient-ui']); // Navigate to Patient UI
-          } else if (this.userRole === 'Admin') {
-            this.router.navigate(['/admin-ui']);
-          } else if (this.userRole === 'Doctor') {
-            this.router.navigate(['/doctor-ui']);
-          }
+          this.userEmail = this.userService.userEmail;
+          this.userRole = this.userService.userRole;
+          this.isAuthenticated = true; // Update authentication state
         }
       },
       error: (error) => {
         console.error('Authentication failed:', error);
-        // Handle errors (e.g., show a message to the user)
       }
     });
   }
 
+  // Navigate to account creation
   onCreateAccountClient() {
     this.router.navigate(['/registerClient']);
   }
 
-  // Toggle the password reset form visibility
+  // Toggle password reset form
   onPasswordReset() {
     this.isPasswordResetMode = true;
+  }
+
+  // Toggle Sidebar Visibility
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
