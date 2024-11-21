ba8e69b847e6cea3da97da35068681122a1d62ce
Gabriel Monteiro <1221029@isep.ipp.pt>
2024-11-21 12:03:13 +0000

    #31 data update

--- TodoApi/Planning-Module/Complexity_Analysis/complexity_analysis.pl
+++ TodoApi/Planning-Module/Complexity_Analysis/complexity_analysis.pl
@@ -66,17 +66,17 @@ surgery(so2,45,60,45).
 surgery(so3,45,90,45).
 surgery(so4,45,75,45).
 
-agenda_staff(d001,20241028,[(720,790,m01),(1080,1140,c01)]).
-agenda_staff(d002,20241028,[(850,900,m02),(901,960,m02),(1380,1440,c02)]).
-agenda_staff(d003,20241028,[(720,790,m01),(910,980,m02)]).
-agenda_staff(d004,20241028,[(850,900,m02),(940,980,c04)]).
+agenda_staff(d001,20241028,[]).
+agenda_staff(d002,20241028,[]).
+agenda_staff(d003,20241028,[]).
+agenda_staff(d004,20241028,[]).
 timetable(d001,20241028,(480,1200)).
 timetable(d002,20241028,(500,1440)).
 timetable(d003,20241028,(520,1320)).
 timetable(d004,20241028,(620,1020)).
 
 
-%surgery_id(so100001,so2).
+surgery_id(so100001,so2).
 %surgery_id(so100002,so3).
 %surgery_id(so100003,so4).
 %surgery_id(so100004,so2).
@@ -90,9 +90,9 @@ timetable(d004,20241028,(620,1020)).
 %surgery_id(so100012,so2).
 surgery_id(so100013,so2).
 
-%assignment_surgery(so100001,d001).
-%assignment_surgery(so100001,d002).
-%assignment_surgery(so100001,d003).
+assignment_surgery(so100001,d001).
+assignment_surgery(so100001,d002).
+assignment_surgery(so100001,d003).
 %assignment_surgery(so100002,d001).
 %assignment_surgery(so100002,d002).
 %assignment_surgery(so100002,d003).
