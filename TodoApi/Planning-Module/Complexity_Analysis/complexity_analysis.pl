da964b2d5a35b9b8ce283f9aa3c0fe5c76a96ae0
Gabriel Monteiro <1221029@isep.ipp.pt>
2024-11-23 15:26:58 +0000

    #41 data update

--- TodoApi/Planning-Module/Complexity_Analysis/complexity_analysis.pl
+++ TodoApi/Planning-Module/Complexity_Analysis/complexity_analysis.pl
@@ -10,19 +10,26 @@
 %%
 % For the Complexity Study
 
-%agenda_staff(d001,20241028,[(720,790,m01),(1080,1140,c01)]).
-%agenda_staff(d002,20241028,[(850,900,m02),(901,960,m02),(1380,1440,c02)]).
-%agenda_staff(d003,20241028,[(720,790,m01),(910,980,m02)]).
-%agenda_staff(d004,20241028,[(850,900,m02),(940,980,c04)]).
+staff(d001,doctor,orthopaedist,[so2,so3,so4]).
+staff(d002,doctor,orthopaedist,[so2,so3,so4]).
+staff(d003,doctor,orthopaedist,[so2,so3,so4]).
+surgery(so2,45,60,45).
+surgery(so3,45,90,45).
+surgery(so4,45,75,45).
 
-%timetable(d001,20241028,(480,1200)).
-%timetable(d002,20241028,(500,1440)).
-%timetable(d003,20241028,(520,1320)).
+agenda_staff(d001,20241028,[(720,790,m01),(1080,1140,c01)]).
+agenda_staff(d002,20241028,[(850,900,m02),(901,960,m02),(1380,1440,c02)]).
+agenda_staff(d003,20241028,[(720,790,m01),(910,980,m02)]).
+%agenda_staff(d004,20241028,[(850,900,m02),(940,980,c04)]).
+timetable(d001,20241028,(480,1200)).
+timetable(d002,20241028,(500,1440)).
+timetable(d003,20241028,(520,1320)).
 %timetable(d004,20241028,(620,1020)).
 
-%surgery_id(so100001,so2).
-%surgery_id(so100002,so3).
-%surgery_id(so100003,so4).
+
+surgery_id(so100001,so2).
+surgery_id(so100002,so3).
+surgery_id(so100003,so4).
 %surgery_id(so100004,so2).
 %surgery_id(so100005,so4).
 %surgery_id(so100006,so2).
@@ -34,9 +41,9 @@
 %surgery_id(so100012,so2).
 %surgery_id(so100013,so2).
 
-%assignment_surgery(so100001,d001).
-%assignment_surgery(so100002,d002).
-%assignment_surgery(so100003,d003).
+assignment_surgery(so100001,d001).
+assignment_surgery(so100002,d002).
+assignment_surgery(so100003,d003).
 %assignment_surgery(so100004,d001).
 %assignment_surgery(so100004,d002).
 %assignment_surgery(so100005,d002).
@@ -53,30 +60,33 @@
 %assignment_surgery(so100013,d004).
 
 
+agenda_operation_room(or1,20241028,[(750, 780, mnt0001),(1080, 1110, mnt0002)]).
+
+
 %%
 
 % Other data
 
-staff(d001,doctor,anesthesia,[so2,so3,so4]).
-staff(d002,doctor,cleaning,[so2,so3,so4]).
-staff(d003,doctor,surgeon,[so2,so3,so4]).
-staff(d004,doctor,surgeon,[so2,so3,so4]).
+%staff(d001,doctor,anesthesia,[so2,so3,so4]).
+%staff(d002,doctor,cleaning,[so2,so3,so4]).
+%staff(d003,doctor,surgeon,[so2,so3,so4]).
+%staff(d004,doctor,surgeon,[so2,so3,so4]).
 
-surgery(so2,45,60,45).
-surgery(so3,45,90,45).
-surgery(so4,45,75,45).
+%surgery(so2,45,60,45).
+%surgery(so3,45,90,45).
+%surgery(so4,45,75,45).
 
-agenda_staff(d001,20241028,[]).
-agenda_staff(d002,20241028,[]).
-agenda_staff(d003,20241028,[]).
-agenda_staff(d004,20241028,[]).
-timetable(d001,20241028,(480,1200)).
-timetable(d002,20241028,(500,1440)).
-timetable(d003,20241028,(520,1320)).
-timetable(d004,20241028,(620,1020)).
+%agenda_staff(d001,20241028,[]).
+%agenda_staff(d002,20241028,[]).
+%agenda_staff(d003,20241028,[]).
+%agenda_staff(d004,20241028,[]).
+%timetable(d001,20241028,(480,1200)).
+%timetable(d002,20241028,(500,1440)).
+%timetable(d003,20241028,(520,1320)).
+%timetable(d004,20241028,(620,1020)).
 
 
-surgery_id(so100001,so2).
+%surgery_id(so100001,so2).
 %surgery_id(so100002,so3).
 %surgery_id(so100003,so4).
 %surgery_id(so100004,so2).
@@ -88,11 +98,11 @@ surgery_id(so100001,so2).
 %surgery_id(so100010,so2).
 %surgery_id(so100011,so4).
 %surgery_id(so100012,so2).
-surgery_id(so100013,so2).
+%surgery_id(so100013,so2).
 
-assignment_surgery(so100001,d001).
-assignment_surgery(so100001,d002).
-assignment_surgery(so100001,d003).
+%assignment_surgery(so100001,d001).
+%assignment_surgery(so100001,d002).
+%assignment_surgery(so100001,d003).
 %assignment_surgery(so100002,d001).
 %assignment_surgery(so100002,d002).
 %assignment_surgery(so100002,d003).
@@ -128,15 +138,16 @@ assignment_surgery(so100001,d003).
 %assignment_surgery(so100012,d001).
 %assignment_surgery(so100012,d002).
 %assignment_surgery(so100012,d003).
-assignment_surgery(so100013,d001).
-assignment_surgery(so100013,d002).
-assignment_surgery(so100013,d003).
-assignment_surgery(so100013,d004).
+%assignment_surgery(so100013,d001).
+%assignment_surgery(so100013,d002).
+%assignment_surgery(so100013,d003).
+%assignment_surgery(so100013,d004).
 
 
 
-agenda_operation_room(or1,20241028,[(750, 780, mnt0001),(1080, 1110, mnt0002)]).
+%agenda_operation_room(or1,20241028,[(750, 780, mnt0001),(1080, 1110, mnt0002)]).
 
+%%
 
 free_agenda0([],[(0,1440)]).
 free_agenda0([(0,Tfin,_)|LT],LT1):-!,free_agenda1([(0,Tfin,_)|LT],LT1).
