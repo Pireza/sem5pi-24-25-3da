8301576422c8c546407db7458a3a63bf8cb896ce
Gabriel Monteiro <1221029@isep.ipp.pt>
2024-12-14 15:03:27 +0000

    #75 e2e tests

---
+++ TodoApi/healthcare-app/cypress/e2e/Doctor/getAllMedicalConditions.cy.ts
@@ -0,0 +1,24 @@
+describe('Create Operation Request Page Tests', () => {
+    beforeEach(() => {
+        cy.visit('/auth');
+        cy.intercept('POST', 'http://localhost:5174/api/Patients/authenticate').as('loginRequest'); // Intercept login request
+        cy.get('#login').click();
+    });
+
+    it('should display the form elements correctly', () => {
+        cy.get('#burger', { timeout: 10000 }).should('be.visible').click();
+        cy.contains('.sidebar-menu li', 'Manage Patient').click();
+        cy.contains('.sidebar-menu li', 'Search Medical Conditions').click();
+
+        // Verify table headers
+        cy.get('table thead tr').within(() => {
+            cy.contains('th', 'Designation').should('exist');
+            cy.contains('th', 'Code').should('exist');
+            cy.contains('th', 'Code System').should('exist');
+            cy.contains('th', 'Description').should('exist');
+            cy.contains('th', 'Common Symptoms').should('exist');
+
+        });
+    });
+
+});
