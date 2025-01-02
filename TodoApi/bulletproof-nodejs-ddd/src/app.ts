5bbe8c3f8f84ce07b0c4fbf1ca82aad70290a504
JoseSilva_1221031 <1221031@isep.ipp.pt>
2025-01-02 16:50:12 +0000

    #80 backend update

--- TodoApi/bulletproof-nodejs-ddd/src/app.ts
+++ TodoApi/bulletproof-nodejs-ddd/src/app.ts
@@ -6,6 +6,8 @@ import allergyRoutes from './api/routes/allergyRoute';
 import medicalConditionsRoutes from './api/routes/medicalConditionsRoute';
 import medicalHistoryRoutes from './api/routes/medicalHistoryRoute'
 import medicalRecordRoutes from './api/routes/medicalRecordRoutes';
+import Appointment from './Models/Appointment';
+import appointmentRoutes from './api/routes/appointmentRoutes';
 const cors = require('cors');
 
 dotenv.config();
@@ -22,6 +24,7 @@ app.use(allergyRoutes);
 app.use(medicalConditionsRoutes);
 app.use(medicalHistoryRoutes);
 app.use(medicalRecordRoutes);
+app.use(appointmentRoutes);
   
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
