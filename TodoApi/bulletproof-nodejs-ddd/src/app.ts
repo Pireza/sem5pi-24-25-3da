9a1a48cade26c64c7f3647f5035850399f990546
Rodrigo Nunes <1220916@isep.ipp.pt>
2025-01-02 18:15:04 +0000

    #7.2.10 back end and front end update

--- TodoApi/bulletproof-nodejs-ddd/src/app.ts
+++ TodoApi/bulletproof-nodejs-ddd/src/app.ts
@@ -5,6 +5,8 @@ import connectDB from './config/db';
 import allergyRoutes from './api/routes/allergyRoute';
 import medicalConditionsRoutes from './api/routes/medicalConditionsRoute';
 import medicalHistoryRoutes from './api/routes/medicalHistoryRoute'
+import roomRoots from './api/routes/roomRoute'
+
 const cors = require('cors');
 
 dotenv.config();
@@ -20,6 +22,7 @@ app.use(bodyParser.json());
 app.use(allergyRoutes);
 app.use(medicalConditionsRoutes);
 app.use(medicalHistoryRoutes);
+app.use(roomRoots);
   
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
